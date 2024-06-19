279b3845953578979a0fe622b97802e56e2d389c
mohnapne <kuvaldin2012@gmail.com>
2024-06-19 22:06:46 +0200

    lesson 40

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Lesson07 from './lessons/lesson_07/Lesson07';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Lesson07 />
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
